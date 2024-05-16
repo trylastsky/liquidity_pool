afa788d6554ac4ce8d72d3fef6eb6f48ac9c02ec
Cloudy_Trylastsky <146930018+trylastsky@users.noreply.github.com>
2024-05-16 22:11:44 +0300

    Update Pool.sol
    Pool reform

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -4,48 +4,130 @@ pragma solidity 0.8.24;
 
 import "./ERC20.sol";
 
+
 contract Pool {
-    uint pollsAmount = 0; // количество пулов в системе изначально
     //назначение токенов внутри pool
-    GerdaCoin public gerdaCoin; 
-    KrendelCoin public krendelCoin;
-    RTKCoin public rtkCoin;
+    GerdaCoin public gerdaCoin; //gerda token
+    KrendelCoin public krendelCoin; // krendel token
+    RTKCoin public rtkCoin; //RTK token
+    Professional public PROFI;// LP token
 
-    struct Gerda_Krendel_pool { // структура пула Gerda -> Krendel
-        uint GerdaAmount; //количество GerdaCoin в пуле
-        uint KrendelAmount; //количество KrendelCoin в пуле
-        address owner; //владелец пула 
-    }
+    address public owner; // владелец пула
 
-    struct Krendel_RTK_pool { // структура пула Krendel -> RTK
-        uint KrendelAmount; //количество Krendel в пуле
-        uint RTKAmount; //количество RTKAmount в пуле
-        address owner; //владелец пула
-    }
-    //получить пулы по айди 
-    mapping(uint => Gerda_Krendel_pool) gerda_krendel_pool_byId; 
-    mapping(uint => Krendel_RTK_pool) krendel_RTK_pool_byId;
-
-    mapping(address => uint256) public stakingBalanceGerda; //вложения пользователя в GerdaCoin
-    mapping(address => uint256) public stakingBalanceKrendel; //вложения пользователя в KrendelCoin
-    mapping(address => uint256) public stakingBalanceRTK; //вложения пользователя в RTKCoin
+    mapping (address => uint) public value_donated_token1; // мапинги для стэйкинга
+    mapping (address => uint) public value_donated_token2; // количество пожертвованных токенов пользователем
 
     constructor(
                 address _gerdaCoinAddress,
                 address _krendelCoinAddress, 
-                address _rtkCoinAddress) {
+                address _rtkCoinAddress,
+                address _owner) {
         //внедрение интерфейсов токенов
         gerdaCoin = GerdaCoin(_gerdaCoinAddress);
         krendelCoin = KrendelCoin(_krendelCoinAddress);
         rtkCoin = RTKCoin(_rtkCoinAddress);
-        
+        owner = _owner;
     } 
-                       
 
-    function add_Gerda_Krendel_pool(uint _startedGerdaAmount, uint _startedKrendelAmount) public {
-        require(gerdaCoin.balanceGerdaCoin(msg.sender) == _startedGerdaAmount &&
-                krendelCoin.balanceKrendelCoin(msg.sender) == _startedKrendelAmount,"insufficient funds to create a pool");
-        gerda_krendel_pool_byId[pollsAmount] = Gerda_Krendel_pool(_startedGerdaAmount, _startedKrendelAmount,msg.sender);
-        pollsAmount++;
+}
+
+
+contract Gerda_Krendel is Pool {
+    constructor(
+                address _gerdaCoinAddress,
+                address _krendelCoinAddress, 
+                address _rtkCoinAddress,
+                address _owner) Pool(
+                    _gerdaCoinAddress,
+                    _krendelCoinAddress, 
+                    _rtkCoinAddress,
+                    _owner) {
+                }
+
+    function add_gerda_for_pool(uint _amount_gerda) internal {// функция добавляющая gerda в pool
+        if (msg.sender == owner) {//если пользователь создатель пула, то бонусы ему не начисляться
+            gerdaCoin.transferGerdaCoin(msg.sender, address(this), _amount_gerda);
+        }
+        else { //иначе начислим бонусы вложившему в пул
+            gerdaCoin.transferGerdaCoin(msg.sender, address(this), _amount_gerda);
+            value_donated_token1[msg.sender] += _amount_gerda;
+        }
+    }
+
+    function add_krendel_for_pool(uint _amount_krendel) internal { //функция добавляющая krendel в pool 
+        if (msg.sender == owner) {//если пользователь создатель пула, то бонусы ему не начисляться
+                krendelCoin.transferKrendelCoin(msg.sender, address(this), _amount_krendel);
+            }
+            else { //иначе начислим бонусы вложившему в пул
+                krendelCoin.transferKrendelCoin(msg.sender, address(this), _amount_krendel);
+                value_donated_token2[msg.sender] += _amount_krendel;
+            }
+    }
+
+    function extend_for_gerda(uint _amount_krendel_to_extend) public {//фукнция обмена gerda на krendel
+        // расчитать код соотношения и написать полноценую функцию обмена
+    }
+
+    function extend_for_krendel(uint _amount_gerda_to_extend) public { //функция обмена krendel на gerda
+        // расчитать код соотношения и написать полноценую функцию обмена
+    }
+
+    function view_balance_pool(address _adr) public view returns(uint gerda_bal,uint krendel_bal) {
+        gerda_bal = gerdaCoin.balanceGerdaCoin(_adr);
+        krendel_bal = krendelCoin.balanceKrendelCoin(_adr);
+    }
+     
+}
+
+
+contract Krendel_RTK is Pool {
+    constructor(
+                address _gerdaCoinAddress,
+                address _krendelCoinAddress, 
+                address _rtkCoinAddress,
+                address _owner) Pool(
+                    _gerdaCoinAddress,
+                    _krendelCoinAddress, 
+                    _rtkCoinAddress,
+                    _owner) {
+                }        
+
+        struct Krendel_RTK_pool { // структура пула Krendel -> RTK
+        uint KrendelAmount; //количество Krendel в пуле
+        uint RTKAmount; //количество RTKAmount в пуле
+        address owner; //владелец пула
+    }             
+
+    function add_krendel_for_pool(uint _amount_krendel) internal {// функция добавляющая krendel в pool
+        if (msg.sender == owner) {//если пользователь создатель пула, то бонусы ему не начисляться
+            krendelCoin.transferKrendelCoin(msg.sender, address(this), _amount_krendel);
+        }
+        else { //иначе начислим бонусы вложившему в пул
+            krendelCoin.transferKrendelCoin(msg.sender, address(this), _amount_krendel);
+            value_donated_token1[msg.sender] += _amount_krendel;
+        }
+    }
+
+    function add_rtk_for_pool(uint _amount_rtk) internal { //функция добавляющая RTK в pool 
+        if (msg.sender == owner) {//если пользователь создатель пула, то бонусы ему не начисляться
+                rtkCoin.transferRTKCoin(msg.sender, address(this), _amount_rtk);
+            }
+            else { //иначе начислим бонусы вложившему в пул
+                rtkCoin.transferRTKCoin(msg.sender, address(this), _amount_rtk);
+                value_donated_token2[msg.sender] += _amount_rtk;
+            }
+    }
+
+    function extend_for_rtk(uint _amount_krendel_to_extend) public {//фукнция обмена krendel на rtk
+        // расчитать код соотношения и написать полноценую функцию обмена
+    }
+
+    function extend_for_krendel(uint _amount_rtk_to_extend) public { //функция обмена rtk на krendel
+        // расчитать код соотношения и написать полноценую функцию обмена
+    }
+
+    function view_balance_pool(address _adr) public view returns(uint gerda_bal,uint krendel_bal) {
+        gerda_bal = gerdaCoin.balanceGerdaCoin(_adr);
+        krendel_bal = krendelCoin.balanceKrendelCoin(_adr);
     }
 }
