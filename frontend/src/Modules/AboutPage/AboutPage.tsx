28571882129c252229dfee60aaa9f343f2ebaf3f
John Doe <johndoe@example.com>
2025-01-15 17:21:20 +0400

    about page

--- frontend/src/Modules/AboutPage/AboutPage.tsx
+++ frontend/src/Modules/AboutPage/AboutPage.tsx
@@ -1,40 +1,41 @@
-import React from 'react';
-import InfiniteScrollIcons from './InfiniteScrollIcons/InfiniteScrollIcons';
-import iconsData from "./IconsData.json";
+import React, { useState } from 'react';
+
+import {ethers} from "ethers";
+import factory_json from "../../../../backend/artifacts/contracts/Factory.sol/Factory.json";
+
 import "./AboutPage.css";
 
 
-import aboutText from "./aboutText.json";
 
 const AboutPage: React.FC = () => {
+    const [pools, setPools] = useState<string[]>([]);
+
+    const handleGetPools = async () => {
+        try {
+            // Подключение к провайдеру (например, Metamask)
+            const provider = new ethers.BrowserProvider(window.ethereum);
+            // Запрос разрешения на доступ к счетам
+            await provider.send("eth_requestAccounts", []);
+
+            // Создайте экземпляр контракта
+            const contract = new ethers.Contract(factory_json.address, factory_json.abi, provider);
+
+            // Вызовите функцию контракта для получения пулов ликвидности
+            const getPools = await contract.getAllAddressPools();
+            setPools(getPools);
+        } catch (error) {
+            console.error("Ошибка при получении пулов:", error);
+        } 
+    };
+
     return (
         <div className='container'>
-            <h1>{aboutText.title}</h1>
-            <div className='textBlock'>
-                <p>
-                    {aboutText.welcomeMessage}
-                </p>
-                <p>
-                    {aboutText.mission}
-                </p>
-                <InfiniteScrollIcons iconsData={iconsData}/>
-                <p>
-                    В LiqPool мы предлагаем:
-                </p>
-                <ul>
-                    {aboutText.offerings.map((offering, index) => (
-                        <li key={index}>
-                            <strong>{offering.strong}</strong> {offering.description}
-                        </li>
-                    ))}
-                </ul>
-                <p>
-                    {aboutText.team}
-                </p>
-                <p>
-                    {aboutText.thankYou}
-                </p>
-            </div>
+                <button onClick={handleGetPools}>Получить пулы ликвидности</button>
+                <div style={{backgroundColor: "red", display: "flex",flexDirection:"column"}}>
+                {pools.map((item,key) => {
+                    return <p key={key}>{item}</p>
+                })}
+                </div>
         </div>
     );
 };
